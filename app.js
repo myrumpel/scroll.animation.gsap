bb59d528bb2dcdb613f99b1649d2e00937b2d96a
Ivan Malovichko <128544015+myrumpel@users.noreply.github.com>
2023-04-27 15:33:53 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,10 +5,18 @@ gsap.from('.b2 h2', {
 	scale: 0.5,
 	scrollTrigger: {
 		trigger: '.b2 h2',
-		markers: true,
+		// markers: true,
 		// Trigger SCroller
 		// default properties
 		start: 'top 80%',
 		end: 'bottom 20%',
+		// scrub: 1,
 	},
+})
+ScrollTrigger.create({
+	trigger: '.b2',
+	pin: true,
+	scrub: true,
+	markers: true,
+	end: 'bottom 50%',
 })
